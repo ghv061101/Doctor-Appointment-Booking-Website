20d456303b0a5f58e60bffe94b69ca005a3e6a74
ghv061101 <ghv061101@gmail.com>
2024-09-29 17:14:18 +0530

    in

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -44,6 +44,7 @@ const About = () => {
 
       
     </div>
+    
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
-import Footer from '../components/Footer'
+
 
 
 const Home = () => {
@@ -13,7 +13,7 @@ const Home = () => {
        <SpecialityMenu/>
        <TopDoctors/>
        <Banner/>
-       <Footer/>
+       
     </div>
   )
 }
